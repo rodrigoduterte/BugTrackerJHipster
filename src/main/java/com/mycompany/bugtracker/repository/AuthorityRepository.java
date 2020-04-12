5db9f2f8b5bc29213ac0fbf2f1a7933223d44ebc
Gabriel Ferrer <gabohermoso@gmail.com>
2020-04-12 00:39:48 -0400

    initial commit

---
+++ src/main/java/com/mycompany/bugtracker/repository/AuthorityRepository.java
@@ -0,0 +1,10 @@
+package com.mycompany.bugtracker.repository;
+
+import com.mycompany.bugtracker.domain.Authority;
+import org.springframework.data.mongodb.repository.ReactiveMongoRepository;
+
+/**
+ * Spring Data MongoDB repository for the {@link Authority} entity.
+ */
+public interface AuthorityRepository extends ReactiveMongoRepository<Authority, String> {
+}
